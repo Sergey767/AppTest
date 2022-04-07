3c5ea2f1cdc251df66b47b445d33f4ae4163b148
Сергей Горячев <serezha_goryachev@list.ru>
2022-04-07 19:07:35 +0400

    создал главный экран

---
+++ CoctailAppTest/CoctailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CoctailViewController.swift
+//  CoctailAppTest
+//
+//  Created by Сергей Горячев on 07.04.2022.
+//
+
+import UIKit
+
+class CoctailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
